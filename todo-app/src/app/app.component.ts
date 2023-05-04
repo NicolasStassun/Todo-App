3fd4cdc8542d23486248ba9754576c0e6aa3d5e2
NICOLAS STASSUN <nicolas_stassun@estudante.sesisenai.org.br>
2023-05-04 09:42:04 -0300

    3.0

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { unsupported } from '@angular/compiler/src/render3/view/util';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { forEach } from '@angular/router/src/utils/collection';
 interface Tarefa{
 
   nome: string;
   descricao: string;
   categoria: string;
-  index: number;
 
 }
 
@@ -16,7 +15,10 @@ interface Tarefa{
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.tarefas = JSON.parse(localStorage.getItem('listaDeTarefas'))
+  }
   title = 'todo-app';
 
   tarefas: Tarefa [] = [];
@@ -27,8 +29,7 @@ export class AppComponent {
 
     nome: '',
     descricao:'',
-    categoria: '',
-    index: null
+    categoria: ''
 
   }
 
@@ -39,10 +40,14 @@ export class AppComponent {
       nome: this.teste.nome,
       descricao: this.teste.descricao,
       categoria: this.teste.categoria,
-      index: this.tarefas.length
       
     };
-    window.localStorage.setItem("nomeObjeto",JSON.stringify(novaTarefa))  
+    let listaLocalStorage = JSON.parse(localStorage.getItem('listaDeTarefas'))
+    if(listaLocalStorage == null)[
+      listaLocalStorage = []
+    ]
+    listaLocalStorage.push(novaTarefa)
+    localStorage.setItem("listaDeTarefas",JSON.stringify(listaLocalStorage))  
 
     this.tarefas.push(novaTarefa);
 
@@ -54,37 +59,44 @@ export class AppComponent {
   deletaTarefa(indice):void{
 
     this.tarefas.splice(indice,1);
-      
+    localStorage.setItem("listaDeTarefas",JSON.stringify(this.tarefas))
+
   }
 
   addCategoriaTODO(teste):string{
 
     return teste.categoria = "TODO"
+    localStorage.setItem("listaDeTarefas",JSON.stringify(this.tarefas))
 
   }
   addCategoriaDOING(teste):string{
 
     return teste.categoria = "DOING"
+    localStorage.setItem("listaDeTarefas",JSON.stringify(this.tarefas))
 
   }
   addCategoriaDONE(teste):string{
 
     return teste.categoria = "DONE"
+    localStorage.setItem("listaDeTarefas",JSON.stringify(this.tarefas))
 
   }
   categoriaTODO(Tarefa):string{
 
     return Tarefa.categoria = "TODO"
+    localStorage.setItem("listaDeTarefas",JSON.stringify(this.tarefas))
 
   }
   categoriaDOING(Tarefa):string{
 
     return Tarefa.categoria = "DOING"
+    localStorage.setItem("listaDeTarefas",JSON.stringify(this.tarefas))
 
   }
   categoriaDONE(Tarefa):string{
 
     return Tarefa.categoria = "DONE"
+    localStorage.setItem("listaDeTarefas",JSON.stringify(this.tarefas))
 
   }
 
